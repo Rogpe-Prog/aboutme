d050c659d4052e29d0d89349ae7f0a069aac0d82
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-07-26 07:57:21 -0300

    books

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,10 +3,9 @@ export const Team = (props) => {
     <div id='team' className='text-center'>
       <div className='container'>
         <div className='col-md-8 col-md-offset-2 section-title'>
-          <h2>Meet the Team</h2>
+          <h2>Books and Articles</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Here, some pages beyond understanding, seeking to precede..
           </p>
         </div>
         <div id='row'>
